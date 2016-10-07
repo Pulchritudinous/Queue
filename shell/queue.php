6b83c5913834f7dc8ad7d4b48ee8359297c2cbdb
Anton Samuelsson <samuelsson.anton@gmail.com>
2016-10-07 22:59:53 +0200

    Started to add some basic features for shell script

--- shell/queue.php
+++ shell/queue.php
@@ -17,20 +17,28 @@ class Pulchritudinous_Queue_Shell
     protected $_processes = [];
 
     /**
+     *
+     *
+     * @var string
+     */
+    protected $_shellFile;
+
+    /**
      * Initialize application and parse input parameters
      */
     public function __construct()
     {
         $this->_parseArgs();
 
-
-
+        register_shutdown_function([$this, 'exitStrategy']);
 
         if ($this->_includeMage) {
             require_once $this->_getRootPath() . 'app' . DIRECTORY_SEPARATOR . 'Mage.php';
             Mage::app($this->_appCode, $this->_appType);
         }
-        $this->_factory = new Mage_Core_Model_Factory();
+
+        $this->_factory     = new Mage_Core_Model_Factory();
+        $this->_shellFile   = __FILE__;
 
         $this->_applyPhpVariables();
         $this->_construct();
@@ -43,15 +51,31 @@ class Pulchritudinous_Queue_Shell
      */
     public function run()
     {
+        if ($id = $this->getArg('labour')) {
+            return $this->_runLabour($id);
+        }
 
+        if ($this->getArg('help')) {
+            echo $this->usageHelp();
+            exit(0);
+        }
+
+        $this->_runServer();
     }
 
     /**
      *
      */
-    protected function _runJob()
+    protected function _runLabour($id)
     {
+        try {
+            $labour = Mage::getModel('pulchqueue/labour')->load($id);
+            $labour->execute();
+        } catch (Exception $e) {
+            exit(1);
+        }
 
+        exit(0);
     }
 
     /**
@@ -59,15 +83,43 @@ class Pulchritudinous_Queue_Shell
      */
     protected function _runServer()
     {
+        $count          = 0;
+        $binfile        = (isset($_SERVER['_'])) ? $_SERVER['_'] : 'php';
+        $shellfile      = $this->_shellFile;
+        $queue          = Mage::getSingleton('pulchqueue/queue');
+        $configModel    = Mage::getSingleton('pulchqueue/config');
+        $configData     = new Varien_Object(
+            Mage::getConfig()->getNode('global/pulchqueue')->asArray()
+        );
+
+        $cwd    = sys_get_temp_dir();
+        $spec   = [
+           ['pipe', 'r'],
+           ['pipe', 'w'],
+           ['pipe', 'w']
+       ];
+
         while (true) {
             $this->_validateProcesses();
 
-            if ($this->_canStartNext()) {
-
+            if (!$this->_canStartNext()) {
+                usleep($configData->getData('queue/poll'));
+                continue;
             }
 
-            sleep(1);
+            $labour     = $queue->reserve();
+            $command    = "{$binfile} {$shellfile} --labour {$labour->getId()}";
+            $resource   = proc_open($command, $spec, $pipes, $cwd);
+            $status     = proc_get_status($resource);
+
+            if ($status && isset($status['pid'])) {
+                $this->_processes[] = $status['pid'];
+            }
+
+            usleep($configData->getData('queue/poll'));
         }
+
+        exit(0);
     }
 
     /**
@@ -78,7 +130,7 @@ class Pulchritudinous_Queue_Shell
     protected function _validateProcesses()
     {
         foreach ($this->_processes as $key => $process) {
-            if (!$this->_validateProcesses($process)) {
+            if (!$this->_validateProcess($process)) {
                 unset($this->_processes[$key]);
             }
         }
@@ -113,6 +165,14 @@ class Pulchritudinous_Queue_Shell
     }
 
     /**
+     * Run state.
+     */
+    public static function exitStrategy()
+    {
+        echo 'Hello World!';
+    }
+
+    /**
      * Helper text.
      *
      * @return string
@@ -130,3 +190,4 @@ USAGE;
 
 $shell = new Pulchritudinous_Queue_Shell();
 $shell->run();
+
