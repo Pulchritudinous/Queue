5d1d77bb3d3221573e90a2933ae02e19e78848b3
Anton Samuelsson <samuelsson.anton@gmail.com>
2016-11-17 21:56:12 +0100

    Added test suite

---
+++ dev/tests/pulchqueue/src/autoload.php
@@ -0,0 +1,15 @@
+<?php
+// Include Magento libraries
+require_once realpath(MAGENTO_ROOT) . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'Mage.php';
+
+if (!Mage::isInstalled()) {
+    echo "Application is not installed yet, please complete install wizard first.";
+    exit;
+}
+
+// Start the Magento application
+Mage::app('admin')->setUseSessionInUrl(false);
+
+// Avoid issues "Headers already send"
+session_start();
+
