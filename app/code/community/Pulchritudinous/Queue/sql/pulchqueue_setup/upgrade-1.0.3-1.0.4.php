6b4275c710590df3c7d257a22bedb40309b441d8
Anton Samuelsson <samuelsson.anton@gmail.com>
2017-10-31 08:29:51 +0100

    Fixed column definition in update script

--- app/code/community/Pulchritudinous/Queue/sql/pulchqueue_setup/upgrade-1.0.3-1.0.4.php
+++ app/code/community/Pulchritudinous/Queue/sql/pulchqueue_setup/upgrade-1.0.3-1.0.4.php
@@ -7,14 +7,14 @@ $adapter    = $this->getConnection();
 $adapter->addColumn(
     $tableName,
     'by_recurring',
-    Varien_Db_Ddl_Table::TYPE_SMALLINT,
-    null,
     [
-        'unsigned'  => true,
+        'type'      => Varien_Db_Ddl_Table::TYPE_SMALLINT,
         'nullable'  => false,
+        'unsigned'  => true,
         'default'   => '0',
-    ],
-    'Is created by recurring'
+        'after'     => 'pid',
+        'comment'   => 'Is created by recurring'
+    ]
 );
 
 $this->endSetup();
